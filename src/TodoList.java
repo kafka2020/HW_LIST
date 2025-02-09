652c5b556787e3b3bf1646c8169c7a4413304d2e
Andrew S <>
2025-02-09 23:49:18 +0300

    Solve the first task

--- src/TodoList.java
+++ src/TodoList.java
@@ -5,17 +5,71 @@ import java.util.Scanner;
 public class TodoList {
 
     public static void main(String[] args) {
-        List<String> list = new LinkedList<>();
+        List<String> tasks = new LinkedList<>();
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             printMenu();
 
-            break;
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+
+            switch (choice) {
+                case 0:
+                    System.out.println("Пока!");
+                    return;
+                case 1:
+                    System.out.print("Введите название задачи: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("Добавлено!");
+                    break;
+                case 2:
+                    printTasks(tasks);
+                    break;
+                case 3:
+                    System.out.print("Введите номер для удаления: ");
+                    int taskNumber = scanner.nextInt();
+                    scanner.nextLine();
+                    if (taskNumber > 0 && taskNumber <= tasks.size()) {
+                        tasks.remove(taskNumber - 1);
+                        System.out.println("Удалено!");
+                    } else {
+                        System.out.println("Нет дела с таким номером!");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Введите задачу для удаления: ");
+                    String taskToDelete = scanner.nextLine();
+                    if (tasks.remove(taskToDelete)) {
+                        System.out.println("Удалено!");
+                    } else {
+                        System.out.println("Нет дела с таким названием!");
+                    }
+                    break;
+                default:
+                    System.out.println("Такой операции нет!");
+            }
+
+            // Вывод текущего списка дел после каждой операции
+            printTasks(tasks);
         }
     }
 
-    public static void printMenu() {
+    public static void printTasks (List < String > tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("Список дел пуст.");
+        } else {
+            System.out.println("Ваш список дел:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+
+    public static void printMenu () {
         System.out.println("Выберите операцию:");
         System.out.println("0. Выход из программы");
         System.out.println("1. Добавить дело");
@@ -25,3 +79,6 @@ public class TodoList {
         System.out.print("Ваш выбор: ");
     }
 }
+
+
+
